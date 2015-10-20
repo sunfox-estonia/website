72adecd46520c339f7340d3f5be3651772729afa
Victor Litvinkov <victor.litvinkov@eesti.ee>
2015-10-20 12:51:03 +0300

    Prepare to paralax on home page.

--- resources/js/virvik.js
+++ resources/js/virvik.js
@@ -19,3 +19,23 @@ $(function() {
     }
   });
 });
+/*
+Home page intro paralax
+More info: http://stackoverflow.com/questions/15465481/is-there-a-way-to-make-parallax-work-within-a-div
+*/
+function parallax() {
+    var ev = {
+        scrollTop: document.body.scrollTop || document.documentElement.scrollTop
+    };
+    ev.ratioScrolled = ev.scrollTop / (document.body.scrollHeight - document.documentElement.clientHeight);
+    render(ev);
+}
+function render(ev) {
+    var t = ev.scrollTop;
+    var y = Math.round(t * 2/3) - 800;
+    // var y = Math.round((1 - ev.ratioScrolled) * -100);
+    $('div#intro_thirdhill').css('background-position', '0 ' + y + 'px');
+    $('div#intro_sechill').css('background-position', '0 ' + y + 'px');
+    $('div#intro_firsthill').css('background-position', '0 ' + y + 'px');
+    $('div#intro_flag').css('background-position', '0 ' + y + 'px');
+}
