4e586cd12e738e05ba5c63fd8ee4f68a48cd5f0f
Victor Litvinkov <victor@viruviking.club>
2015-10-20 22:51:48 +0300

    Parallax prepare.

--- resources/js/virvik.js
+++ resources/js/virvik.js
@@ -19,3 +19,20 @@ $(function() {
     }
   });
 });
+/* Parallax effect */
+function parallax() {
+    var ev = {
+        scrollTop: document.body.scrollTop || document.documentElement.scrollTop
+    };
+    ev.ratioScrolled = ev.scrollTop / (document.body.scrollHeight - document.documentElement.clientHeight);
+    render(ev);
+}
+function render(ev) {
+    var t = ev.scrollTop;
+    var y = Math.round(t * 2/3);
+    // $('.intro').css('background-position', '0 ' + y + 'px');
+    $('div.intro_thirdhill').css('background-position', '0 ' + y+1 + 'px');
+    $('div.intro_sechill').css('background-position', '0 ' + y+5 + 'px');
+    $('div.intro_firsthill').css('background-position', '0 ' + y + 'px');
+    $('div.intro_flag').css('background-position', '0 ' + y + 'px');
+}
