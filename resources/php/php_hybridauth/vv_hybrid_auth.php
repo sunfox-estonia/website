42dbb21e04b87a515cc5eb2e4bc7dccc184bf75d
Victor Litvinkov <victor.litvinkov@eesti.ee>
2016-02-11 23:59:24 +0200

    Oauth new version.

--- resources/php/php_hybridauth/vv_hybrid_auth.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once( "/var/www/admin/www/v2.viruviking.club/resources/php/php_hybridauth/config.php" );
-require_once( "/var/www/admin/www/v2.viruviking.club/resources/php/php_hybridauth/Hybrid/Auth.php" );
-
-$hybridauth = new Hybrid_Auth( $config_file_path );
-$provider_name = $_GET["provider"];
-
-// See more example: http://hybridauth.sourceforge.net/userguide/Integrating_HybridAuth_Social_Login.html
