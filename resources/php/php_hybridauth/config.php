4003ba8124096d2a89b598643208dc05b26841fd
Victor Litvinkov <victor.litvinkov@eesti.ee>
2016-02-11 18:19:35 +0200

    Add hybridauth authorization script. Prepare for coding.

---
+++ resources/php/php_hybridauth/config.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * HybridAuth
+ * http://hybridauth.sourceforge.net | http://github.com/hybridauth/hybridauth
+ * (c) 2009-2015, HybridAuth authors | http://hybridauth.sourceforge.net/licenses.html
+ */
+// ----------------------------------------------------------------------------------------
+//	HybridAuth Config file: http://hybridauth.sourceforge.net/userguide/Configuration.html
+// ----------------------------------------------------------------------------------------
+
+return
+		array(
+			"base_url" => "https://".$_SERVER['SERVER_NAME']."resources/php/php_hybridauth/",
+			"providers" => array(
+				// openid providers
+				"Google" => array(
+					"enabled" => true,
+					"keys" => array("id" => "", "secret" => ""),
+				),
+				"Facebook" => array(
+					"enabled" => true,
+					"keys" => array("id" => "", "secret" => ""),
+					"trustForwarded" => false
+				),
+				"Vkontakte" => array(
+					"enabled" => true,
+					"keys" => array("id" => "", "secret" => "")
+				),
+			),
+			// If you want to enable logging, set 'debug_mode' to true.
+			// You can also set it to
+			// - "error" To log only error messages. Useful in production
+			// - "info" To log info and error messages (ignore debug messages)
+			"debug_mode" => true,
+			// Path to file writable by the web server. Required if 'debug_mode' is not false
+			"debug_file" => "vv_user_auth.log",
+);
