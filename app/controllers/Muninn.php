1a7bff9fca3fab85aeac7a347284e86f564d2833
Vitgor Sunfox <vitgor@sunfox.ee>
2018-09-14 19:52:57 +0300

    Bot updates

--- app/controllers/Muninn.php
+++ app/controllers/Muninn.php
@@ -12,10 +12,11 @@ class vkcom {
     const VK_API_ENDPOINT = 'https://api.vk.com/method/';
     
     // Log settings
-    const BOT_LOGS_DIRECTORY = '.'; 
+    const BOT_LOGS_DIRECTORY = '/var/www/virvik/sunfox.ee/app/controllers'; 
     
     
     protected function _callback_getRequest() {
+        error_log(file_get_contents('php://input'), 0);
         return json_decode(file_get_contents('php://input'));
     }
     
@@ -33,62 +34,86 @@ class vkcom {
     }
   
     /* Send message to user */
-    protected function vkApi_messagesSend($peer_id, $message) {
-        return _vkApi_call('messages.send', array(
-          'peer_id'    => $peer_id,
-          'message'    => $message,
+    protected function vkApi_messagesSend($user_id, $message) {
+        return $this->_vkApi_call('messages.send', array(
+          'user_id'    => $user_id,
+          'message'    => $message
         ));
+        error_log("Message sent", 0);
     }
+    
+    
+    public function vkApi_keyboardSend($user_id, $message, $commands=[], $one_time = False) {
+        $keyboard_prep = [];
+        $i = 0;
+        foreach ($commands as $button_str) {
+            $j = 0;
+            foreach ($button_str as $button) {
+                $color = $button[2];
+                $keyboard_prep[$i][$j]["action"]["type"] = "text";
+                if ($button[0] != null)
+                    $keyboard_prep[$i][$j]["action"]["payload"] = json_encode($button[0], JSON_UNESCAPED_UNICODE);
+                $keyboard_prep[$i][$j]["action"]["label"] = $button[1];
+                $keyboard_prep[$i][$j]["color"] = $color;
+                $j++;
+            }
+            $i++;
+        }
+        $keyboard_fin = array(
+            "one_time" => $one_time,
+            "buttons" => $keyboard_prep);        
+        $keyboard_fin = json_encode($keyboard_fin, JSON_UNESCAPED_UNICODE);
+        return $this->_vkApi_call('messages.send', array(
+          'user_id'    => $user_id,
+          'message'    => $message,
+          'keyboard'   => $keyboard_fin
+        ));
+        error_log("Message + keyboard sent", 0);
+    }
+
     
     /* Get iser data by id */
     protected function vkApi_usersGet($user_id) {
-        return _vkApi_call('users.get', array(
+        return $this->_vkApi_call('users.get', array(
           'user_id' => $user_id,
         ));
     }
     
-    private function _vkApi_call($method, $params = array()) {
-        $params['access_token'] = self::VK_API_ACCESS_TOKEN;
-        $params['v'] = self::VK_API_VERSION;
-        $query = http_build_query($params);
-        $url = self::VK_API_ENDPOINT.$method.'?'.$query;
-        $curl = curl_init($url);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        $json = curl_exec($curl);
-        $error = curl_error($curl);
-        if ($error) {
-          log_error($error);
-          throw new Exception("Failed {$method} request");
-        }
-        curl_close($curl);
-        $response = json_decode($json, true);
-        if (!$response || !isset($response['response'])) {
-          log_error($json);
-          throw new Exception("Invalid response for {$method} request");
-        }
-        return $response['response'];
-    }    
     
-    protected function log_msg($message) {
-        if (is_array($message)) {
-          $message = json_encode($message);
+    private function _vkApi_call($method,$params=array()){
+        $url = 'https://api.vk.com/method/'.$method;
+        $params['access_token']=self::VK_API_ACCESS_TOKEN;
+        $params['v']=self::VK_API_VERSION;
+        if (function_exists('curl_init')) {
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+                "Content-Type:multipart/form-data"
+            ));
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
+            $json = curl_exec($ch);
+            $error = curl_error($ch);
+            if ($error) {
+              error_log($json, 0);
+              throw new Exception("Failed {$method} request");
+            }
+            curl_close($ch);
+        } else {
+            $json = file_get_contents($url, true, stream_context_create(array(
+                'http' => array(
+                    'method'  => 'POST',
+                    'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+                    'content' => http_build_query($params)
+                )
+            )), true);
         }
-        _log_write('[INFO] ' . $message);
-    }
-    
-    protected function log_error($message) {
-        if (is_array($message)) {
-          $message = json_encode($message);
-        }
-        _log_write('[ERROR] ' . $message);
-    }
-    
-    protected function _log_write($message) {
-        $trace = debug_backtrace();
-        $function_name = isset($trace[2]) ? $trace[2]['function'] : '-';
-        $mark = date("H:i:s") . ' [' . $function_name . ']';
-        $log_name = self::BOT_LOGS_DIRECTORY.'/munin_log.txt';
-        file_put_contents($log_name, $mark . " : " . $message . "\n", FILE_APPEND);
+        $response = json_decode($json, TRUE);
+        if (isset($response['response']))
+            return $response['response'];
+        else
+            error_log($json, 0);
+            throw new Exception("Invalid response for {$method} request");
     }
 }
 
@@ -98,10 +123,11 @@ class vkcom {
  * Community events: https://vk.com/dev/groups_events
  * Keyboard: https://vk.com/dev/bots_docs_3?f=4.%2B%D0%9A%D0%BB%D0%B0%D0%B2%D0%B8%D0%B0%D1%82%D1%83%D1%80%D1%8B%2B%D0%B4%D0%BB%D1%8F%2B%D0%B1%D0%BE%D1%82%D0%BE%D0%B2
  * Additional docs & examples: https://game-tips.ru/it/kak-sdelat-knopki-byistryih-otvetov-keyboard-dlya-botov-v-vk/#php_code
+ *
+ * Check log here: /var/www/logs/php-fpm/error.log
  */
-define('BTN_EVENTS', '[["command" => "show_events"], "Мероприятия", "default"]');
-
 class Muninn extends vkcom {
+    const BTN_EVENTS = [["command" => 'show_events'], "Смотреть мероприятия", "default"];
     
     public function go(){        
         if (!isset($_REQUEST)) {
@@ -113,6 +139,7 @@ class Muninn extends vkcom {
     // Chat Requests handler
     public function callback_handleEvent() {
         $request = $this->_callback_getRequest();
+        
         try {
             switch ($request->type) {
             //Подтверждение сервера
@@ -121,39 +148,53 @@ class Muninn extends vkcom {
                 break;
             //Вступление в группу
             case 'group_join':
-                $user_id = $request->object->user_id;                
+                $req_user = $request->object->user_id;  
+                $req_peer_id = $request->object->id;     
                 $message = "Благодарим за интерес к Викингам Вирумаа! Напиши, если желаешь посетить пробное занятие или задать вопросы :) ";
-                $this->vkApi_messagesSend($user_id, $message);
+                $this->vkApi_messagesSend($req_user, $message);
                 break;
             //Уход из группы
             case 'group_leave':
                 
                 break;
             case 'message_new':
-                $this->_log_write($request->object);
-                //$user_id = $request->object->user_id;                
-                //$message = "Благодарим за за сообщение! Мы скоро ответим!";
-                //$this->vkApi_messagesSend($user_id, $message);
+                $req_user = $request->object->user_id;  
+                $req_peer_id = $request->object->id;  
+                $req_payload = ($request->object->payload) ? json_decode($data->object->payload, true) : null;
+                if ($req_payload){
+                    // Run commands script
+                    break;
+                }                    
+                $req_msg = mb_strtolower($request->object->body);
                 
-                //$user_id = $request->object->from_id; 
-                //$payload = ($request->object->payload) ? json_decode($data->object->payload, true) : null;                           
-                //$message = "Данный функционал (" . var_dump($payload)  . "), к сожалению, не доступен.";
-                //$this->vkApi_messagesSend($user_id, $message);
+                if($req_msg == 'начать'|| $req_msg == 'start'){
+                    $message = "Рады что ты откликнулся! :) Чтобы познакомиться ближе с Викингами, посети одно из предстоящих наших мероприятий.";
+                    $this->vkApi_keyboardSend($req_user, $message, [[self::BTN_EVENTS]], $one_time=TRUE);
+                    break;
+                }
+                
+                    
+                /* {"type":"message_new","object":
+                *    {   "id":430,
+                *        "date":1536918642,
+                *        "out":0,
+                *        "user_id":102501707,
+                *        "read_state":0,
+                *        "title":"",
+                *        "body":"dfg"},
+                *   "group_id":85213325,
+                *    "secret":"NTuAN9p5bST7pmqW"
+                *    }
+                */ 
                 break;
             default:
                 $this->_callback_response('Unsupported event');
                 break;
             }
         } catch (Exception $e) {
-            log_error($e);
+            error_log($e, 0);
         }
         $this->_callback_okResponse();
     }
-    
-    protected function get_user($object){
-        $users_get_response = $this->vkApi_usersGet($object['user_id']);
-        $user_data = array_pop($users_get_response);
-        return $user_data;
-    }   
     
 }
